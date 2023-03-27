7793857ab35ffbad2d5babb8319ac368f481d789
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-03-28 00:26:37 +0200

    Fixed minor bug

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${minutes}`;
-  }
   let minutes = date.getMinutes();
   if (minutes < 10) {
   minutes = `0${minutes}`;
   }
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${minutes}`;
+  }
+  
   let days = [
     "Sunday",
     "Monday",
